7b862ffe45ce1219933cb61546764ec2858063ef
Higashi-Kota <higashi.kota@gri.jp>
2022-04-06 19:04:58 +0900

    update reference

--- basic-fauna-fql-operation/deleteDocument.js
+++ basic-fauna-fql-operation/deleteDocument.js
@@ -3,7 +3,14 @@ import { client, q } from './config';
 (async () => {
   try {
     let response;
-    response = await client.query(q.Delete(q.Documents(q.Collection('Rss'))));
+    response = await client.query(
+      q.Map(
+        q.Paginate(q.Documents(q.Collection('Rss'))),
+        q.Lambda('X', q.Delete(q.Var('X')))
+      )
+    );
+    // NG
+    // response = await client.query(q.Delete(q.Documents(q.Collection('Rss'))));
     console.log(response);
   } catch (error) {
     console.log(error);
